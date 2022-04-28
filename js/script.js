13c1082da8d6cee9df00d5f966c34173610f857f
Chandni <chandni1141996@gmail.com>
2022-04-28 10:58:39 +0530

    added margin to hero section to fix browser jumping issue, added condition to remove sticky class and added root margin to intersection observer

--- js/script.js
+++ js/script.js
@@ -48,13 +48,18 @@ const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
     if (!ent.isIntersecting) {
-      document.querySelector(".header").classList.add("sticky");
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
     }
   },
   {
     // In the viewport
     root: null,
     threshold: 0,
+    rootMargin: "-80px",
   }
 );
 obs.observe(sectionHeroEl);
